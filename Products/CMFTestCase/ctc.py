81e34b24a05f4d2019a639722ba3068bc9db6d39
Stefan H. Holek <stefan@epy.co.at>
2005-02-25 11:29:25 +0000

    Add module exposing the public API. Will simplify imports in the future.
    svn path=/trunk/; revision=2075

---
+++ Products/CMFTestCase/ctc.py
@@ -0,0 +1,24 @@
+#
+# CMFTestCase public interface
+#
+
+from Testing.ZopeTestCase import hasProduct
+from Testing.ZopeTestCase import installProduct
+
+from Testing.ZopeTestCase import Sandboxed
+from Testing.ZopeTestCase import Functional
+
+from Testing.ZopeTestCase import utils
+from Testing.ZopeTestCase.utils import *
+
+from Products.CMFTestCase.setup import portal_name
+from Products.CMFTestCase.setup import portal_owner
+from Products.CMFTestCase.setup import default_products
+from Products.CMFTestCase.setup import default_user
+from Products.CMFTestCase.setup import default_password
+
+from Products.CMFTestCase.setup import setupCMFSite
+
+from Products.CMFTestCase.CMFTestCase import CMFTestCase
+from Products.CMFTestCase.CMFTestCase import FunctionalTestCase
+
