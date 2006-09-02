05db6d3627d9f34303149109be82f7abde2e96b5
Stefan H. Holek <stefan@epy.co.at>
2006-09-02 19:10:30 +0000

    Prettify.
    svn path=/CMFTestCase/trunk/; revision=29767

--- Products/CMFTestCase/tests.py
+++ Products/CMFTestCase/tests.py
@@ -1,21 +1,16 @@
 import unittest
-import os, sys
-
-import os, sys
+import os
+import Products.CMFTestCase
 
 suite = unittest.TestSuite()
 
-if __name__ == '__main__':
-    execfile(os.path.join(sys.path[0], 'framework.py')) 
-
 names = os.listdir(os.path.dirname(__file__))
-tests = [x[:-3] for x in names \
-         if x.startswith('test') and x.endswith('.py') \
-         and not x == 'tests.py']
+tests = [x[:-3] for x in names
+         if x.startswith('test') and x.endswith('.py')
+         and x != 'tests.py']
 
-import Products.CMFTestCase
 for test in tests:
-    m = __import__("Products.CMFTestCase.%s" %test)
+    m = __import__('Products.CMFTestCase.%s' % test)
     m = getattr(Products.CMFTestCase, test)
     if hasattr(m, 'test_suite'):
         suite.addTest(m.test_suite())
