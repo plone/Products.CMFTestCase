946729c1aa776543b650d61ae8213ac2cb963424
Stefan H. Holek <stefan@epy.co.at>
2007-06-23 22:17:40 +0000

    Use new GS API.
    svn path=/CMFTestCase/trunk/; revision=44436

--- Products/CMFTestCase/CMFTestCase.py
+++ Products/CMFTestCase/CMFTestCase.py
@@ -130,12 +130,16 @@ class CMFTestCase(PortalTestCase):
             installed = getattr(self.portal, '_installed_profiles', {})
             if not installed.has_key(name):
                 setup = self.portal.portal_setup
-                saved = setup.getImportContextID()
-                try:
-                    setup.setImportContext('profile-%s' % (name,))
-                    setup.runAllImportSteps()
-                finally:
-                    setup.setImportContext(saved)
+                profile_id = 'profile-%s' % (name,)
+                if CMF21:
+                    setup.runAllImportStepsFromProfile(profile_id)
+                else:
+                    saved = setup.getImportContextID()
+                    try:
+                        setup.setImportContext(profile_id)
+                        setup.runAllImportSteps()
+                    finally:
+                        setup.setImportContext(saved)
                 self._refreshSkinData()
         finally:
             setSecurityManager(sm)
